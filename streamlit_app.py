ec52f958d4594549bab7336880b72039517514d7
osmarsandoval2003-maker <osmarsandoval2003@gmail.com>
2025-09-18 11:43:32 -0600

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,8 @@ if ingredient_list:
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_responde.json(),use_container_width=True)
 
 
 
