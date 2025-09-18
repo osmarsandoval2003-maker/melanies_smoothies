973176f0127da8118504156b5342d9b33f93961f
osmarsandoval2003-maker <osmarsandoval2003@gmail.com>
2025-09-18 12:11:34 -0600

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,8 +21,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 fruit_list = [row["FRUIT_NAME"] for row in my_dataframe.collect()]
 
 # Ingredient selection
@@ -44,7 +44,8 @@ if ingredient_list:
         INSERT INTO smoothies.public.orders(ingredients, name_on_order)
         VALUES ('{ingredients_string}', '{name_on_order}')
     """
-
+    search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+    st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
     # Submit button
     time_to_insert = st.button("Submit Order")
 
